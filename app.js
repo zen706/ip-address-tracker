d3f10c39d5410268d0bc450b7f613f22cecf72b7
zen706 <aanon5313@gmail.com>
2022-06-06 14:44:16 +0900

    domain unavailable

--- app.js
+++ app.js
@@ -13,37 +13,56 @@ const result = document.querySelector('.result')
 let map
 
 $.getJSON('//api.ipify.org?format=jsonp&callback=?', function (data) {
-  const API = `//ip-api.com/json/${data.ip}`
+  // const API = `//ip-api.com/json/${data.ip}`  //over https unavailable
+  // const API = `https://ipapi.co/${data.ip}/json/` //domain unavailable
+  // const API=`https://geo.ipify.org/api/v2/country,city?apiKey=at_vHasExd2Dgin4pbT7YpU10jS8qnoF&ipAddress=${data.ip}` //reached to limit
+  const API = `https://api.ipgeolocation.io/ipgeo?apiKey=b660ef56ed1e45c29bcfd0e02a7cd35a&ip=${data.ip}` ////domain unavailable
   log(API)
 
   fetchData(API)
 })
 
 const fetchData = async (api) => {
-  try{
+  try {
     const res = await fetch(api)
     const data = await res.json()
+    log(data)
     setMap(data)
     displayInfo(data)
-  }
-  catch(err) {
+  } catch (err) {
     console.log(err)
   }
 }
 
-function displayInfo({ query, city, regionName, countryCode,zip, timezone, isp }) {
-  resultIp.innerHTML = query
-  resultLocation.innerHTML = `${city}, ${regionName}, ${countryCode}, ${zip}`
-  resultTimezone.innerHTML = timezone
+function displayInfo({
+  ip,
+  city,
+  state_prov,
+  country_code2,
+  zipcode,
+  time_zone,
+  isp,
+}) {
+  resultIp.innerHTML = ip
+  resultLocation.innerHTML = `${city}, ${state_prov}, ${country_code2}, ${zipcode}`
+  resultTimezone.innerHTML = `UTC ${
+    Math.abs(time_zone.offset) < 10
+      ? `${
+          time_zone.offset >= 0
+            ? `+0${time_zone.offset}`
+            : `-0${Math.abs(time_zone.offset)}`
+        }`
+      : `${time_zone.offset >= 0 ? `+${time_zone.offset}` : time_zone.offset}`
+  }:00`
   resultIsp.innerHTML = isp
 }
 
-function setMap({ lat, lon }) {
+function setMap({ latitude, longitude }) {
   if (map) {
     map.remove()
   }
-  map = L.map('map').setView([lat, lon], 13)
-  const marker = L.marker([lat, lon]).addTo(map)
+  map = L.map('map').setView([latitude, longitude], 13)
+  const marker = L.marker([latitude, longitude]).addTo(map)
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap',
@@ -54,7 +73,8 @@ search.addEventListener('submit', (e) => {
   e.preventDefault()
   if (ValidateIPaddress(input.value)) {
     log('success')
-    const API = `http://ip-api.com/json/${input.value}`
+    const API = `https://api.ipgeolocation.io/ipgeo?apiKey=b660ef56ed1e45c29bcfd0e02a7cd35a&ip=${input.value}`
+
     fetchData(API)
   } else {
     log('failure')
@@ -93,15 +113,13 @@ function ValidateIPaddress(ipAddress) {
   }
 }
 
-document.querySelector('body').addEventListener('mouseover',(e)=>{
+document.querySelector('body').addEventListener('mouseover', (e) => {
   toggleHideResult(e)
 })
 
-const toggleHideResult=(e)=>{
+const toggleHideResult = (e) => {
   const id = e.target.id
-  if (
-    id === 'map' 
-  ) {
+  if (id === 'map') {
     result.classList.add('hide')
   } else {
     result.classList.remove('hide')
